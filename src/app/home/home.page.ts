ec2f0438dffc0e27fd26b3736520e2e25cbc8234
Murillo Nahás <murillonahas@hotmail.com>
2024-05-11 09:53:19 -0300

    feat: work on CRUD

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { DataService, Note } from '../services/data.service';
+import { AlertController, ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,60 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  notes: Note[] = [];
 
-  constructor() {}
+  constructor(
+    private service: DataService,
+    private cd: ChangeDetectorRef,
+    private alertCtrl: AlertController,
+    private modalCtrl: ModalController
+  ) {
+    this.service.getNotes().subscribe((res) => {
+      this.notes = res;
+      this.cd.detectChanges();
+    });
+  }
 
+  async addNote() {
+    const alert = await this.alertCtrl.create({
+      header: 'Add Note',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'My cool note',
+          type: 'text',
+        },
+        {
+          name: 'text',
+          placeholder: 'Learn Ionic',
+          type: 'textarea',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Add',
+          handler: (res) => {
+            this.service.addNote({ text: res.text, title: res.title });
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async openNote(note: Note) {
+    const modal = await this.modalCtrl.create({
+      component: ModalPage,
+      componentProps: { id: note.id },
+      breakpoints: [0, 0.5, 0.8],
+      initialBreakpoint: 0.8,
+    });
+
+    await modal.present();
+  }
 }
--- src/app/modal/modal.page.ts
+++ src/app/modal/modal.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DataService, Note } from '../services/data.service';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.page.scss'],
 })
 export class ModalPage implements OnInit {
+  @Input() id!: string;
+  note!: Note;
 
-  constructor() { }
+  constructor(
+    private service: DataService,
+    private modalCtrl: ModalController,
+    private toastCtrl: ToastController
+  ) {}
 
   ngOnInit() {
+    this.service.getNoteById(this.id).subscribe((res) => {
+      this.note = res;
+    });
   }
 
+  async deleteNote() {
+    await this.service.deleteNote(this.note);
+    this.modalCtrl.dismiss();
+  }
+
+  async updateNote() {
+    await this.service.updateNote(this.note);
+    const toast = await this.toastCtrl.create({
+      message: 'Note updated!.',
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
